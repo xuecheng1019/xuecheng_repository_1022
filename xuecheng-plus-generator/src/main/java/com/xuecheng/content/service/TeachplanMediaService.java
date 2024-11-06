33a386b34759aaf789a820cbefa08690513267b4
jiez31 <2655159078@qq.com>
2024-11-06 21:53:13 +0800

    高并发测试存在service工程下

---
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/content/service/TeachplanMediaService.java
@@ -0,0 +1,16 @@
+package com.xuecheng.content.service;
+
+import com.xuecheng.content.model.po.TeachplanMedia;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author itcast
+ * @since 2024-11-05
+ */
+public interface TeachplanMediaService extends IService<TeachplanMedia> {
+
+}
